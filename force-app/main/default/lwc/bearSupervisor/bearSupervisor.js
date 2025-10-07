5190c558eaf6357062501dfa8bd1c2018c342fd7
Adarsh Maharjan <a-maharjan@ryowahouse.co.jp>
2025-10-07 17:46:49 +0900

    chore: add custom components

---
+++ force-app/main/default/lwc/bearSupervisor/bearSupervisor.js
@@ -0,0 +1,13 @@
+import { getFieldValue, getRecord } from "lightning/uiRecordApi";
+import { LightningElement, api, wire } from "lwc";
+import SUPERVISOR_FIELD from "@salesforce/schema/Bear__c.Supervisor__c";
+
+const bearFields = [SUPERVISOR_FIELD];
+export default class BearSupervisor extends LightningElement {
+  @api recordId;
+  @wire(getRecord, { recordId: "$recordId", fields: bearFields }) bear;
+
+  get supervisorId() {
+    return getFieldValue(this.bear.data, SUPERVISOR_FIELD);
+  }
+}
