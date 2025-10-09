ecfb064ac1108f3eef575462fbea77ed04b6d0e0
Adarsh Maharjan <a-maharjan@ryowahouse.co.jp>
2025-10-09 15:41:12 +0900

    feat: add bear map

--- force-app/main/default/lwc/bearList/bearList.js
+++ force-app/main/default/lwc/bearList/bearList.js
@@ -1,14 +1,26 @@
 import { NavigationMixin } from "lightning/navigation";
 import { LightningElement, wire } from "lwc";
+import BEAR_LIST_UPDATE_MESSAGE from "@salesforce/messageChannel/BearListUpdate__c";
 
 import ursusResources from "@salesforce/resourceUrl/ursus_park";
 /** BearController.searchBears(searchTerm) Apex method */
 import searchBears from "@salesforce/apex/BearController.searchBears";
+import { MessageContext, publish } from "lightning/messageService";
 export default class BearList extends NavigationMixin(LightningElement) {
   searchTerm = "";
-  @wire(searchBears, { searchTerm: "$searchTerm" })
+  // @wire(searchBears, { searchTerm: "$searchTerm" })
   bears;
-
+  @wire(MessageContext) messageContext;
+  @wire(searchBears, { searchTerm: "$searchTerm" })
+  loadBears(result) {
+    this.bears = result;
+    if (result.data) {
+      const message = {
+        bears: result.data
+      };
+      publish(this.messageContext, BEAR_LIST_UPDATE_MESSAGE, message);
+    }
+  }
   appResources = {
     bearSilhouette: `${ursusResources}/standing-bear-silhouette.png`
   };
